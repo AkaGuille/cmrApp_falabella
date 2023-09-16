ae03ab16391da646398b29d30f1c75938d846202
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2023-09-16 16:34:44 -0500

    React app initialized

---
+++ my-react-app/src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div className="iniciar-sesin">
+        <div className="div">
+          <div className="sesion-button">
+            <div className="overlap-group">
+              <div className="text-wrapper">Iniciar Sesión</div>
+            </div>
+          </div>
+          <div className="text-wrapper-2">¿Olvidaste tu contraseña?</div>
+          <div className="contrasea-button">
+            <div className="overlap">
+              <div className="text-wrapper-3">Contraseña</div>
+              <img
+                className="iconsax-bold"
+                alt="Iconsax bold"
+                src="eyeslash.svg"
+              />
+            </div>
+          </div>
+          <div className="email-button">
+            <div className="overlap">
+              <div className="text-wrapper-3">Correo Electrónico</div>
+            </div>
+          </div>
+          <div className="text-wrapper-4">¡Bienvenido de vuelta!</div>
+          <img
+            className="banco-falabella"
+            alt="Banco falabella"
+            src="banco-falabella.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
